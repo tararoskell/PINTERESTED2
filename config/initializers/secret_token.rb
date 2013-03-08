30246cb4aa702ccd638ff84b9f4c41508243caff
Tara Roskell <tara@troskell.demon.co.uk>
2013-03-08 16:17:23 +0000

    FIRST COMMIT

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PINTERESTED2::Application.config.secret_token = '6b8cb8200b019ca61705b8a35ab827b3e3720db83e342ce95bcdb71ebd5dbba75d407436d26c801306e093d698ed9bd53fdd1bc8fec5e8b5876d83e038aed249'
